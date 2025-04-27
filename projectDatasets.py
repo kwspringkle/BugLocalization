d8652d3ec3ca36f57b32e9101e21a6306fe3b566
Quynh <101940232+kwspringkle@users.noreply.github.com>
2025-04-27 17:57:12 +0700

    Update projectDatasets.py

--- projectDatasets.py
+++ projectDatasets.py
@@ -4,14 +4,13 @@ from pathlib import Path
 # Dataset root directory
 _DATASET_ROOT = Path('/content/drive/MyDrive/Colab Notebooks/BugLocalization/Dataset')
 
-
 Dataset = namedtuple('Dataset', ['name', 'src', 'bug_repo', 'repo_url', 'features'])
 
 # Source codes and bug repositories
 
 aspectj = Dataset(
     'aspectj',
-    _DATASET_ROOT / 'source files/org.aspectj-bug43351/',
+    _DATASET_ROOT / 'source files/org.aspectj-bug433351/',
     _DATASET_ROOT / 'bug reports/AspectJ.txt',
     "https://github.com/eclipse/org.aspectj/tree/bug433351.git",
     _DATASET_ROOT / 'features_aspectj.csv'
@@ -35,7 +34,7 @@ swt = Dataset(
 
 tomcat = Dataset(
     'tomcat',
-    _DATASET_ROOT / 'source files/tomcat-7.0.51/',
+    _DATASET_ROOT / 'source files/tomcat-7.0.51',
     _DATASET_ROOT / 'bug reports/Tomcat.txt',
     "https://github.com/apache/tomcat.git",
     _DATASET_ROOT / 'features_tomcat.csv/'
@@ -43,11 +42,8 @@ tomcat = Dataset(
 
 birt = Dataset(
     'birt',
-    _DATASET_ROOT / 'source files/birt-20140211-1400.zip',
+    _DATASET_ROOT / 'source files/birt-20140211-1400',
     _DATASET_ROOT / 'bug reports/Birt.txt',
     "https://github.com/apache/birt.git",
     _DATASET_ROOT / 'features_birt.csv'
 )
-
-
-
